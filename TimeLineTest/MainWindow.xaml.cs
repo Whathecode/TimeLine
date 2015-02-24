0bb68dd758deb417638f57912f859172ec29a66a
Steven Jeuris <steven.jeuris@gmail.com>
2015-02-24 11:24:37 +0100

    Added styling for TimeIndicator.

--- TimeLineTest/MainWindow.xaml.cs
+++ TimeLineTest/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using Whathecode.AxesPanels.Controls;
 using Whathecode.System.Arithmetic.Range;
 
@@ -22,7 +25,15 @@ namespace TimeLineTest
 			InitializeComponent();
 
 			TimeControlItem test = new TimeControlItem();
-			test.Content = new Label { Content = "Occurance is working!" };
+			test.Content = new Border
+			{
+				Background = Brushes.SteelBlue,
+				Width = 100,
+				Height = 50,
+				CornerRadius = new CornerRadius( 5 ),
+				BorderBrush = Brushes.White,
+				BorderThickness = new Thickness( 2 )
+			};
 			test.Occurance = new DateTime( 2015, 2, 23 );
 			Items.Add( test );
 		}
--- Whathecode.AxesPanels/Controls/TimeLineItem.cs
+++ Whathecode.AxesPanels/Controls/TimeLineItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using Whathecode.System.Windows.DependencyPropertyFactory;
 using Whathecode.System.Windows.DependencyPropertyFactory.Attributes;
 
@@ -11,19 +12,46 @@ namespace Whathecode.AxesPanels.Controls
 	{
 		public enum TimeLineItemBinding
 		{
-			Occurance
+			Occurance,
+			Fill,
+			Stroke,
+			StrokeThickness
 		}
 
 
 		static readonly Type Type = typeof( TimeLineItem );
 		public static readonly DependencyPropertyFactory<TimeLineItemBinding> PropertyFactory = new DependencyPropertyFactory<TimeLineItemBinding>();
 		public static readonly DependencyProperty OccuranceProperty = PropertyFactory[ TimeLineItemBinding.Occurance ];
+		public static readonly DependencyProperty FillProperty = PropertyFactory[ TimeLineItemBinding.Fill ];
+		public static readonly DependencyProperty StrokeProperty = PropertyFactory[ TimeLineItemBinding.Stroke ];
+		public static readonly DependencyProperty StrokeThicknessProperty = PropertyFactory[ TimeLineItemBinding.StrokeThickness ];
 
 		[DependencyProperty( TimeLineItemBinding.Occurance )]
 		public DateTime Occurance
 		{
 			get { return (DateTime)PropertyFactory.GetValue( this, TimeLineItemBinding.Occurance ); }
 			set { PropertyFactory.SetValue( this, TimeLineItemBinding.Occurance, value ); }
+		}
+
+		[DependencyProperty( TimeLineItemBinding.Fill )]
+		public Brush Fill
+		{
+			get { return (Brush)PropertyFactory.GetValue( this, TimeLineItemBinding.Fill ); }
+			set {  PropertyFactory.SetValue( this, TimeLineItemBinding.Fill, value ); }
+		}
+
+		[DependencyProperty( TimeLineItemBinding.Stroke )]
+		public Brush Stroke
+		{
+			get { return (Brush)PropertyFactory.GetValue( this, TimeLineItemBinding.Stroke ); }
+			set {  PropertyFactory.SetValue( this, TimeLineItemBinding.Stroke, value ); }
+		}
+
+		[DependencyProperty( TimeLineItemBinding.StrokeThickness )]
+		public double StrokeThickness
+		{
+			get { return (double)PropertyFactory.GetValue( this, TimeLineItemBinding.StrokeThickness ); }
+			set {  PropertyFactory.SetValue( this, TimeLineItemBinding.StrokeThickness, value ); }
 		}
 
 
