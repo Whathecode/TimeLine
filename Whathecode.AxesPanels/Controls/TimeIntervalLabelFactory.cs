653b01b61b0324d843ddd6f3022eabe099c8b51c
Steven Jeuris <steven.jeuris@gmail.com>
2015-02-26 18:29:57 +0100

    Allow specifying format string for time labels.

--- Whathecode.AxesPanels/Controls/TimeIntervalLabelFactory.cs
+++ Whathecode.AxesPanels/Controls/TimeIntervalLabelFactory.cs
@@ -6,9 +6,15 @@ namespace Whathecode.AxesPanels.Controls
 {
 	public class TimeIntervalLabelFactory : AbstractXAxesFactory<DateTime, TimeSpan, double, double>
 	{
+		/// <summary>
+		///   A DateTime format string, determining how to represent the occurance.
+		/// </summary>
+		public string LabelFormat { get; set; }
+
+
 		protected override FrameworkElement CreateLabel()
 		{
-			return new TimeLineLabel { Interval = StepSize };
+			return new TimeLineLabel { Interval = StepSize, LabelFormat = LabelFormat };
 		}
 
 		protected override void InitializeLabel( PositionedElement positioned, AxesIntervals<DateTime, TimeSpan, double, double> visible )
--- Whathecode.AxesPanels/Controls/TimeLineLabel.cs
+++ Whathecode.AxesPanels/Controls/TimeLineLabel.cs
@@ -10,20 +10,32 @@ namespace Whathecode.AxesPanels.Controls
 	{
 		public enum LabelProperties
 		{
-			Interval
+			Interval,
+			LabelFormat
 		}
 
 
 		static readonly Type Type = typeof( TimeLineLabel );
 		public static readonly DependencyPropertyFactory<LabelProperties> Factory = new DependencyPropertyFactory<LabelProperties>();
 		public static readonly DependencyProperty IntervalProperty = Factory[ LabelProperties.Interval ];
+		public static readonly DependencyProperty LabelFormatProperty = Factory[ LabelProperties.LabelFormat ];
 
 
 		[DependencyProperty( LabelProperties.Interval )]
 		public TimeSpan Interval
 		{
 			get { return (TimeSpan)Factory.GetValue( this, LabelProperties.Interval ); }
-			set {  Factory.SetValue( this, LabelProperties.Interval, value ); }
+			set { Factory.SetValue( this, LabelProperties.Interval, value ); }
+		}
+
+		/// <summary>
+		///   A DateTime format string, determining how to represent the occurance.
+		/// </summary>
+		[DependencyProperty( LabelProperties.LabelFormat )]
+		public string LabelFormat
+		{
+			get { return (string)Factory.GetValue( this, LabelProperties.LabelFormat ); }
+			set { Factory.SetValue( this, LabelProperties.LabelFormat, value ); }
 		}
 
 
